9d779a7982ac1923483d33e672f6a9e4836ec372
EduardoAraujoB <ea0123450@gmail.com>
2020-03-28 16:29:48 -0300

    created mutation to create a user

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,3 +1,5 @@
+import bcrypt from 'bcryptjs';
+
 import { Prisma } from '../prisma/generated/prisma-client';
 
 const prisma = new Prisma();
@@ -5,5 +7,17 @@ const prisma = new Prisma();
 export default {
   Query: {
     users: () => prisma.users(),
+  },
+  Mutation: {
+    user: async (_parent: null, { name, email, password }) => {
+      const password_hash = await bcrypt.hash(password, 10);
+      const user = await prisma.createUser({
+        name,
+        email,
+        password: password_hash,
+      });
+
+      return user;
+    },
   },
 };
