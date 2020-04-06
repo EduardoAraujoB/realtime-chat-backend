1f4765d4c63a1062a536c6c55f870204e637c06b
EduardoAraujoB <ea0123450@gmail.com>
2020-04-06 16:23:18 -0300

    created a root resolver

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,25 +1,11 @@
-import bcrypt from 'bcryptjs';
-
-import { Prisma } from '../prisma/generated/prisma-client';
-
 import UserQuerys from './queries/user';
-
-const prisma = new Prisma();
+import UserMutations from './mutations/user';
 
 export default {
   Query: {
     ...UserQuerys,
   },
   Mutation: {
-    user: async (_parent: null, { name, email, password }) => {
-      const password_hash = await bcrypt.hash(password, 10);
-      const user = await prisma.createUser({
-        name,
-        email,
-        password: password_hash,
-      });
-
-      return user;
-    },
+    ...UserMutations,
   },
 };
