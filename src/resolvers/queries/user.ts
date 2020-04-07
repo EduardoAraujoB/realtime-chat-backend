2e2d96d080ae5d142de2db3675d7c159bbcdb2b6
EduardoAraujoB <ea0123450@gmail.com>
2020-04-07 14:22:38 -0300

    populating users with your chats

--- src/resolvers/queries/user.ts
+++ src/resolvers/queries/user.ts
@@ -2,7 +2,9 @@ import { Prisma } from '../../prisma/generated/prisma-client';
 
 const prisma = new Prisma();
 
+import UserWithChats from '../../prisma/fragments/UserWithChats';
+
 export default {
-  users: () => prisma.users(),
-  user: (_parent: null, { id }) => prisma.user({ id }),
+  users: () => prisma.users().$fragment(UserWithChats),
+  user: (_parent: null, { id }) => prisma.user({ id }).$fragment(UserWithChats),
 };
